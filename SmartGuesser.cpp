b961a41376fb2cc9dd7fa20be9dbdc9bdb67099f
snir <46107190+snirSha@users.noreply.github.com>
2019-04-28 15:55:31 +0300

    Update SmartGuesser.cpp

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -1,17 +1,39 @@
-
 #include "SmartGuesser.hpp"
 #include <string>
-
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void bullpgia::SmartGuesser::learn(string reply) {
-    cout<<"learning"<<endl;
+int* bullpgia::SmartGuesser::invalidChoicesArray(string& str){
+
+    for(int i=0; i<str.length();i++){
+        this->arr[(str[i]-48)] = -1;
+    }
+    for(int i=0; i<10; i++){
+        cout<<"-1 for bad numbers and 0 for numbers left to guess with"<<endl;
+        cout<< arr[i] << "\t";
+    }
+
+    return arr;
 }
 
-void bullpgia::SmartGuesser::startNewGame(uint length) {
-    cout<<" start new game(smartGuesser.cpp)"<<endl;
+void bullpgia::SmartGuesser::learn(string reply) {
+
+    this->bull = reply[0];
+    this->pgia = reply[2];
+    if(this->bull==0 && this->pgia==0){
+        invalidChoicesArray(this->clientGuess);
+    }
+    cout<<"last game was:reply[2] "+to_string(bull)+","+to_string(pgia) <<endl;
+}
+
+void bullpgia::SmartGuesser::startNewGame(uint Length) {
+    this->length=Length;
+    cout<<"Starting new game: "<<endl;
 }
 
 string bullpgia::SmartGuesser::guess() {
+
+
     return "";
 }
