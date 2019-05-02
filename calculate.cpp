fa48933da8a658a859912748b61ae0ed7edc8bb5
snir <46107190+snirSha@users.noreply.github.com>
2019-05-02 12:15:47 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -4,19 +4,27 @@ using namespace std;
 
 namespace bullpgia{
 string calculateBullAndPgia(const string &choice, const string &guess) {
-        uint bull=0;
-    uint pgia=0;
-
-    for(int i =0; i<choice.length();i++){
-        for(int j=0; j<guess.length(); j++){
-            if(i==j && choice[i]==guess[j]){
-                ++bull;
-            }else if(i!=j && choice[i]==guess[j]){
-                ++pgia;
+        string temp = choice;
+        string temp2 = guess;
+        unsigned int bull = 0;
+        unsigned int pgia = 0;
+        for (int i = 0; i < temp.length(); i++) {
+            if (temp[i] == temp2[i]) {
+                bull++;
+                temp[i] = 'a';
+                temp2[i] = 'z';
             }
         }
+        for (int i = 0; i < temp.length(); i++) {
+            for (int j = 0; j < temp2.length(); j++) {
+                if (i != j && temp[i] == temp2[j]) {
+                    pgia++;
+                    temp[i] = 'a';
+                    temp2[j] = 'z';
+                }
+            }
+        }
+        return to_string(bull) + "," + to_string(pgia);
     }
 
-    return to_string(bull)+","+to_string(pgia);
-    }
 }
