ef2e7d98e31d538dd1e31449e42d9260f3e3cc63
snir <46107190+snirSha@users.noreply.github.com>
2019-05-02 16:39:31 +0300

    Update DummyChoosers.cpp

--- DummyChoosers.cpp
+++ DummyChoosers.cpp
@@ -1,7 +1,9 @@
 #include "DummyChoosers.hpp"
 #include <stdlib.h>
+#include <ctime>
 
 std::string RandomChooser::choose(uint length) {
+	srand(time(NULL));
 	std::string r="";
 	for (uint i=0; i<length; ++i) {
 		char c = '0' + (rand()%10);
